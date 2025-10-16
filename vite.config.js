895f165ba0f8382e7feb68931091642704b379bd
Soniya-1995 <soniyarvind.diwakar@gmail.com>
2025-10-16 14:30:40 +0530

    Fix Vite base path for root deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-food-del-app-frontend/',
   plugins: [react()
   ],
+  base: '/', 
 })
